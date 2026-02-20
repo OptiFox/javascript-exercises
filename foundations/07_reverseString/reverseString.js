8a93e479e3665ad0594e3cd756bac1a3f353ed4c
OptiFox <80470592+OptiFox@users.noreply.github.com>
2026-02-21 06:47:11 +0800

    Complete 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function (strings) {
+    // let splitStr = strings.split('');
+    // let reverseStr = splitStr.reverse();
+    // let joinStr = reverseStr.join('');
 
+    // return joinStr;
+
+    // Made it simpler
+    return strings.split('')
+        .reverse()
+        .join('');
 };
 
 // Do not edit below this line
