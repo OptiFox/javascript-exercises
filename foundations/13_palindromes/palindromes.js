34385e6474371729ba2ce3242d0fdfb1c7f9d8f7
OptiFox <80470592+OptiFox@users.noreply.github.com>
2026-03-17 23:39:55 +0800

    Complete 13_palindromes
    What I did:
    - using replaceAll() function to replace all the punctuations.
    - split then join to remove whitespaces.
    - toLowerCase() to make sure everything is in lower case.
    
    What I did wrong:
    - hard coding every punctuations.
    
    What I should've done:
    - whitelisting alphanumerical characters, just the solution provided.
    - this could save me from unknown punctuations user might enter.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let palindrome = str
+    .replaceAll(",", "")
+    .replaceAll("!", "")
+    .replaceAll(".", "")
+    .split(" ")
+    .join("")
+    .toLowerCase();
 
+  return palindrome === palindrome.split("").reverse().join("");
 };
 
 // Do not edit below this line
