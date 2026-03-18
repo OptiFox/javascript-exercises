50d52d9e2560b4271df0c3ae3378d9eada97a5d7
OptiFox <80470592+OptiFox@users.noreply.github.com>
2026-03-18 09:40:31 +0800

    Complete 14_fibonacci
    Quite proud of how simple and clean this one looks,
    but there were some guessing involved, so not too proud of my solution.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (n < 0) return "OOPS";
 
+  let a = 0;
+  let b = 1;
+
+  for (let i = 0; i < n; i++) {
+    a = b + a;
+
+    [a, b] = [b, a];
+  }
+
+  return a;
 };
 
 // Do not edit below this line
