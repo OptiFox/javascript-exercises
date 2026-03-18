ff0fc6dc27f3036f29c68abe38560a98c5198ad6
OptiFox <80470592+OptiFox@users.noreply.github.com>
2026-03-19 01:56:09 +0800

    Complete 16_findTheOldest
    Wasn't easy, but I managed to do it using sort function,
    and I learned that I can just write something like
    sortByAge[0] to get the value of the first object, then
    sortByAge[0].name to get the name of the first object.
    
    Quite a valuable lesson.

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  let sortByAge = arr.sort((a, b) => {
+    if (a.yearOfDeath == null || b.yearOfDeath == null) {
+      b.yearOfDeath = new Date().getFullYear();
+    }
 
+    let aAge = a.yearOfDeath - a.yearOfBirth;
+    let bAge = b.yearOfDeath - b.yearOfBirth;
+
+    return bAge - aAge;
+  });
+
+  return sortByAge[0];
 };
 
 // Do not edit below this line
